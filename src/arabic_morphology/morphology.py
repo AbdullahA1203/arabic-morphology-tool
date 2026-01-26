70534f02913163a97de1d25fff3a30eee1a464f1
Abdullah Ahmad <aahmad1209@outlook.com>
2026-01-26 11:49:06 +0000

    chore: configure OpenAI client using environment variable
    Access environment variable and pass to OpenAI client

--- src/arabic_morphology/morphology.py
+++ src/arabic_morphology/morphology.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 from openai import OpenAI
 
+OPEN_AI_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPEN_AI_KEY)
 
 class FlashCard(BaseModel):
     front: str

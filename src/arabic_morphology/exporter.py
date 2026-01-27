45992cad24a2aaaf29eae774c4fce82e163df87c
Abdullah Ahmad <aahmad1209@outlook.com>
2026-01-27 15:50:21 +0000

    feat: create spreadsheet after pressing 'w'
    Create spreadsheet containing the flashcards generated.

--- src/arabic_morphology/exporter.py
+++ src/arabic_morphology/exporter.py
@@ -1,11 +1,24 @@
 import xlsxwriter
+import os
 
-row = 0
 
 def write_to_sheet(cards):
+
+    row = 0
+
+    workbook = xlsxwriter.Workbook('words.xlsx')
+    worksheet = workbook.add_worksheet()
     
     for card in cards:
+
         card_front = card.front
         card_back = card.back
 
- 
+        worksheet.write(row, 0, card.front)
+        worksheet.write(row, 1, card.back)
+
+        row+=1
+    
+    workbook.close()
+    
+    print("Saved to:", os.path.abspath("words.xlsx"))
